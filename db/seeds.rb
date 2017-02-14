25cec64e594ba5a4b9d697a98189f8944c7b35a9
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-14 16:22:51 +0000

    Articles controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::Lorem.sentence
+  content = Faker::Lorem.paragraph
+  Article.create(title: title, content: content)
+end
